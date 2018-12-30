c67eaccb12be53a1c72ac8e2ced6acb3810258ac
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2018-12-30 20:33:55 +0530

    Minor changes

--- src/is.utils.ts
+++ src/is.utils.ts
@@ -1,16 +1,7 @@
 const types = [
-  52.365,
-  12,
-  NaN,
-  'name',
-  new Date(),
-  /\d+/,
-  null,
-  true,
-  false,
-  undefined,
-  { a: 10, b: 20 },
-  ['red', 'blue', 'green'],
+    52.365, 12, NaN, 'name', new Date(), /\d+/,
+    null, true, false, undefined,
+    { a: 10, b: 20 }, ['red', 'blue', 'green']
 ];
 
 export const isDate = value => value instanceof Date;
@@ -25,37 +16,37 @@ export const isFalse = value => value === false;
 
 export const isString = value => typeof value === 'string';
 
-/*
+/* 
  * If the value is of Number type?
  * It can be Integer or Float
  */
 export const isPureNumber = value => typeof value === 'number';
 
-/*
+/* 
  * If the value is numeric only?
  * Type can be String or Number
  * It can be Integer or Float
  */
 export const isNumber = value => /^\d+(\.\d+)?$/.test(`${value}`);
 
-/*
+/* 
  * If the value is Float only? i.e., consist decimal value
  * Type can be String or Number
  */
 export const isFloat = value => /^\d+\.\d+$/.test(`${value}`);
 
-/*
+/* 
  * If the value is of Number type and consist decimal value
  */
 export const isPureFloat = value => typeof value === 'number' && isFloat(value);
 
-/*
+/* 
  * If the value is Integer only? i.e., does not consist decimal value
  * Type can be String or Number
  */
 export const isInteger = value => /^\d+$/.test(`${value}`);
 
-/*
+/* 
  * If the value is of Number type and does not consist decimal value
  */
 export const isPureInteger = value => typeof value === 'number' && isInteger(value);
@@ -65,16 +56,14 @@ export const isRegex = value => value instanceof RegExp;
 export const isArray = value => Array.isArray(value);
 
 export const isObject = value => {
-  return (
-    value !== null &&
-    value !== undefined &&
-    !isArray(value) &&
-    !isRegex(value) &&
-    !isDate(value) &&
-    typeof value === 'object'
-  );
+    return value !== null
+        && value !== undefined
+        && !isArray(value)
+        && !isRegex(value)
+        && !isDate(value)
+        && typeof value === 'object';
 };
 
 export const isEmptyObject = value => {
-  return isObject(value) && Object.keys(value).length === 0;
+    return isObject(value) && Object.keys(value).length === 0;
 };
--- src/random.utils.ts
+++ src/random.utils.ts
@@ -2,62 +2,62 @@ import { ALPHAS, DUMMY_TEXT_MIN_4 } from './common.utils';
 import { swapNumbers } from './helpers.utils';
 
 export const randomItem = (list: any[]): any => {
-  return list[Math.floor(Math.random() * list.length)];
+    return list[Math.floor(Math.random() * list.length)];
 };
 
 export const randomAlphabet = (): string => {
-  return randomItem(ALPHAS.toLowerCase().split(''));
+    return randomItem(ALPHAS.toLowerCase().split(''));
 };
 
 export const randomNum = (min: number = 0, max: number = 10): number => {
-  if (min > max) {
-    const { a, b } = swapNumbers(min, max);
-    min = a;
-    max = b;
-  }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (min > max) {
+        const { a, b } = swapNumbers(min, max);
+        min = a;
+        max = b;
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 export const randomAlphaNum = (format): string => {
-  return format.replace(/./g, char => {
-    if (char === 'a') {
-      return randomAlphabet();
-    } else if (char === 'A') {
-      return randomAlphabet().toUpperCase();
-    } else if (char === 'x') {
-      return randomNum(0, 9);
-    } else {
-      return char;
-    }
-  });
+    return format.replace(/./g, char => {
+        if (char === 'a') {
+            return randomAlphabet();
+        } else if (char === 'A') {
+            return randomAlphabet().toUpperCase();
+        } else if (char === 'x') {
+            return randomNum(0, 9);
+        } else {
+            return char;
+        }
+    });
 };
 
 export const randonWords = (min: number = 1, max: number = 1): string => {
-  let i = min;
-  const words: string[] = [];
+    let i = min,
+        words: string[] = [];
 
-  if (min !== max) {
-    max = randomNum(min, max);
-  }
+    if (min !== max) {
+        max = randomNum(min, max);
+    }
 
-  while (i <= max) {
-    words.push(randomItem(DUMMY_TEXT_MIN_4));
-    i++;
-  }
+    while (i <= max) {
+        words.push(randomItem(DUMMY_TEXT_MIN_4));
+        i++;
+    };
 
-  return words.join(' ');
+    return words.join(' ');
 };
 
 export const randomDate = (start, end) => {
-  let startTime = start.getTime(),
-    endTime = end.getTime();
+    let startTime = start.getTime(),
+        endTime = end.getTime();
 
-  if (startTime > endTime) {
-    const { a, b } = swapNumbers(startTime, endTime);
-    startTime = a;
-    endTime = b;
-  }
+    if (startTime > endTime) {
+        const { a, b } = swapNumbers(startTime, endTime);
+        startTime = a;
+        endTime = b;
+    }
 
-  return new Date(startTime + Math.random() * (endTime - startTime));
+    return new Date(startTime + Math.random() * (endTime - startTime));
 };
-// randomDate(new Date(2012, 0, 1), new Date())
